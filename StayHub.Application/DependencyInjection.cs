53b45f5623ae967c7ec6842d8831ae72a5146cef
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2026-07-27 11:29:53 +0300

    feat(application): add DependencyInjection configuration
    - Introduce `DependencyInjection` class for registering application services.
    - Configure MediatR with behaviors and license key.
    - Add FluentValidation validators and register `PricingService` for dependency injection.

---
+++ StayHub.Application/DependencyInjection.cs
@@ -0,0 +1,30 @@
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StayHub.Application.Abstractions.Behaviors;
+using StayHub.Domain.Bookings;
+
+namespace StayHub.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.LicenseKey = configuration["MediatR:LicenseKey"];
+
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+        services.AddTransient<PricingService>();
+
+        return services;
+    }
+}
