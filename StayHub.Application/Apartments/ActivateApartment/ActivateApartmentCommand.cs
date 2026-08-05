f4f6a8ae382a364904ced4b7950a3f14d273f18e
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2026-08-05 10:32:28 +0300

    refactor(application): use `IUserContext` for authorization in `ActivateApartmentCommandHandler`
    - Removed `RequestedByUserId` from `ActivateApartmentCommand`.
    - Updated `ActivateApartmentCommandHandler` to leverage `IUserContext` for user ID validation.

--- StayHub.Application/Apartments/ActivateApartment/ActivateApartmentCommand.cs
+++ StayHub.Application/Apartments/ActivateApartment/ActivateApartmentCommand.cs
@@ -2,4 +2,4 @@ using StayHub.Application.Abstractions.Messaging;
 
 namespace StayHub.Application.Apartments.ActivateApartment;
 
-public sealed record ActivateApartmentCommand(Guid ApartmentId, Guid RequestedByUserId) : ICommand;
+public sealed record ActivateApartmentCommand(Guid ApartmentId) : ICommand;
