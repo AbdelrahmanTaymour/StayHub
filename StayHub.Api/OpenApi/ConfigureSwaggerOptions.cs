ee03abd3e651aa0818d61b701a875c20096e1c42
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2026-08-10 10:46:16 +0300

    feat(api): add `ConfigureSwaggerOptions` to set up API versioning in Swagger
    - Implemented `ConfigureSwaggerOptions` class to generate Swagger documentation for API versions.
    - Added support for marking deprecated API versions in OpenAPI documentation.

---
+++ StayHub.Api/OpenApi/ConfigureSwaggerOptions.cs
@@ -0,0 +1,35 @@
+using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace StayHub.Api.OpenApi;
+
+public sealed class ConfigureSwaggerOptions(
+    IApiVersionDescriptionProvider provider)
+    : IConfigureNamedOptions<SwaggerGenOptions>
+{
+    public void Configure(SwaggerGenOptions options)
+    {
+        foreach (var description in provider.ApiVersionDescriptions)
+            options.SwaggerDoc(description.GroupName, CreateVersionInfo(description));
+    }
+
+    public void Configure(string? name, SwaggerGenOptions options)
+    {
+        Configure(options);
+    }
+
+    private static OpenApiInfo CreateVersionInfo(ApiVersionDescription description)
+    {
+        var openApiInfo = new OpenApiInfo
+        {
+            Title = $"StayHub.Api v{description.ApiVersion}",
+            Version = description.ApiVersion.ToString()
+        };
+
+        if (description.IsDeprecated) openApiInfo.Description += " This API version has been deprecated.";
+
+        return openApiInfo;
+    }
+}
