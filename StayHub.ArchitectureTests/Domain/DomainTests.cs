dab4766d8b050c4007fa96fc68b01d8afe82af64
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2026-09-13 10:45:24 +0300

    test(architecture): add validation tests for domain rules
    - Added `DomainTests` to enforce architectural rules within the Domain layer:
      - Ensured all domain events are sealed and have the `DomainEvent` postfix in their names.
      - Verified that entity classes have private parameterless constructors.

---
+++ StayHub.ArchitectureTests/Domain/DomainTests.cs
@@ -0,0 +1,59 @@
+using System.Reflection;
+using FluentAssertions;
+using NetArchTest.Rules;
+using StayHub.ArchitectureTests.Infrastructure;
+using StayHub.Domain.Abstractions;
+
+namespace StayHub.ArchitectureTests.Domain;
+
+public class DomainTests : BaseTest
+{
+    [Fact]
+    public void DomainEvents_Should_BeSealed()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .That()
+            .ImplementInterface(typeof(IDomainEvent))
+            .Should()
+            .BeSealed()
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DomainEvents_Should_HaveDomainEventsPostfix()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .That()
+            .ImplementInterface(typeof(IDomainEvent))
+            .Should()
+            .HaveNameEndingWith("DomainEvent")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Entities_Should_HavePrivateParameterlessConstructor()
+    {
+        IEnumerable<Type> entityTypes = Types.InAssembly(DomainAssembly)
+            .That()
+            .Inherit(typeof(Entity))
+            .GetTypes();
+
+        var failingTypes = new List<Type>();
+        foreach (var entityType in entityTypes)
+        {
+            ConstructorInfo[] constructors = entityType.GetConstructors(BindingFlags.NonPublic |
+                                                                        BindingFlags.Instance);
+
+            if (!constructors.Any(c => c.IsPrivate && c.GetParameters().Length == 0))
+            {
+                failingTypes.Add(entityType);
+            }
+        }
+
+        failingTypes.Should().BeEmpty();
+    }
+}
